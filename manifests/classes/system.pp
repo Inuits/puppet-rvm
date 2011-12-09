57673db66b4b786a24088085435bcaa452ce043a
Jan Vansteenkiste <jan@vstone.eu>
2011-12-09 10:27:23 +0100

    Fix running the rvm-installer

--- manifests/classes/system.pp
+++ manifests/classes/system.pp
@@ -4,7 +4,7 @@ class rvm::system($version='latest') {
 
   exec { 'system-rvm':
     path    => '/usr/bin:/usr/sbin:/bin',
-    command => "bash -c '/usr/bin/curl -s https://rvm.beginrescueend.com/install/rvm -o /tmp/rvm-installer ; chmod +x /tmp/rvm-installer ; rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man ./tmp/rvm-installer --version ${version}'",
+    command => "bash -c '/usr/bin/curl -s https://rvm.beginrescueend.com/install/rvm -o /tmp/rvm-installer ; chmod +x /tmp/rvm-installer ; rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man . /tmp/rvm-installer --version ${version}'",
     creates => '/usr/local/rvm/bin/rvm',
     require => [
       Class['rvm::dependencies'],
